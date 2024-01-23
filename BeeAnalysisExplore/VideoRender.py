b43a6310d81c047669ac79d542fe6726c0b6d7de
Elias Xu <flam0799@gmail.com>
2024-01-23 11:22:46 -0500

    work on image processing and determing cv2 is ass

---
+++ BeeAnalysisExplore/VideoRender.py
@@ -0,0 +1,24 @@
+"""
+? Can we render these videos correctly though?
+"""
+import os
+import cv2
+
+path = os.path.join("2021-10-13 04:32:22.416976.h264")
+cap = cv2.VideoCapture(path)
+count = 0
+while cap.isOpened():
+    ret, frame = cap.read()
+    
+    if frame is None:
+        break;
+    if not ret:
+        break
+    if ret and frame is not None:
+        count += 1
+        cv2.imshow("frame", frame)
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    
+cap.release()
+cv2.destroyAllWindows()
