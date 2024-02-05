1ad6aab71f7145ab4624d8575d835c8675998f8b
Elias Xu <flam0799@gmail.com>
2024-02-04 19:57:25 -0500

    start work

---
+++ BeeAnalysisExplore/finalFunction.py
@@ -0,0 +1,11 @@
+import os
+import cv2
+
+def processVideo(IN_PATH, OUT_PATH):
+    """
+    There are multiple steops this function
+    1. Read in the video file
+        - scaling to 
+    2. Apply a scale effect
+    3. Crop the video file
+    """
